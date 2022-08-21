a6b32874b77fb9bc078e100b254bb116deb16c06
rizary <andika.riyan@gmail.com>
2022-08-21 22:37:54 +0700

    clean up project

--- docs/markdown/toc.md
+++
@@ -1,7 +0,0 @@
-# Table of Content
-
-- [About The Project](./pages/about.md)
-- [Revenue Stream](./pages/revenue.md)
-- [Governance](./pages/governance.md)
-- [The Business Canvas Model](./pages/bcm.md)
-- [Roadmap](./pages/roadmap.md)
--- frontend/apps/postcss.config.js
+++ frontend/apps/postcss.config.js
@@ -15,7 +15,7 @@ const isProd = env === "production";
 const purgecss = require('@fullhuman/postcss-purgecss')({
 
     // Specify the paths to all of the template files in your project
-    content: ['./src/*.rs'],
+    content: ['./src/*.rs', './src/**/*.rs', './js/wallet.js'],
 
     keyframes: true,
 
