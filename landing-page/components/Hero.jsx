39a2c18394211ff0277eb85382f9250d170a38c8
mirzavic4869 <avicenamirzam@gmail.com>
2022-11-10 11:33:42 +0700

    add font, create hero

--- landing-page/components/Hero.jsx
+++ landing-page/components/Hero.jsx
@@ -6,9 +6,9 @@ const Hero = () => {
 			<div className="hero-content flex-col lg:flex-row-reverse">
 				<img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
 				<div>
-					<h1 className="text-5xl font-bold">Box Office News!</h1>
-					<p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-					<button className="btn btn-primary">Get Started</button>
+					<h1 className="text-5xl font-impact">The First Halal NFTs Collection</h1>
+					<p className="py-6 font-semibold">That Brings Sharia Based DeFi Ecosystem.</p>
+					<button className="btn btn-primary">Downloads Whitepaper</button>
 				</div>
 			</div>
 		</div>
--- landing-page/tailwind.config.js
+++ landing-page/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				impact: ["Impact", "sans-serif"],
+				lato: ["Lato", "sans-serif"],
+			},
+		},
 	},
 	plugins: [require("daisyui")],
 };
