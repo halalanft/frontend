3f8a818370fc6e7fff7edb800ef519849bb9c468
mirzavic4869 <avicenamirzam@gmail.com>
2022-11-20 16:06:32 +0700

    checking all page

--- landing-page/components/RoadMap.jsx
+++ landing-page/components/RoadMap.jsx
@@ -1,61 +1,75 @@
 import React from "react";
+import Image from "next/image";
+import divide from "../assets/divide.png";
 
 const RoadMap = () => {
 	return (
-		<div className="px-8 py-16 min-h-screen w-screen">
+		<div className="px-8 py-16 min-h-screen w-screen bg-[url('../assets/roadmap.png')] lg:bg-cover max-sm:bg-contain">
 			<h1 className="text-4xl font-impact mb-8 text-center">Roadmap to Halalan Ecosystem</h1>
 			<p className="mb-16 text-center lg:w-1/2 mx-auto">Our commitment to build Halal ecosystem around DeFi is really strong. We are aiming to have the following roadmap get realized in timely manner</p>
-			<div>
-				<ol style={{ listStyleType: "number" }}>
-					<li>Project setup</li>
-					<li>Whitepaper</li>
-					<li>Social media setup: Discord, Twitter, Medium</li>
-					<li>NFTs Concept & Design</li>
-					<li>Pre-launch Project</li>
-					<li>Pre-launch Campaign</li>
-					<li>Whitelisting</li>
-					<li>VCs package offering</li>
-					<li>1st AMA</li>
-				</ol>
+
+			<div className="grid grid-cols-3 items-center">
+				<div className="justify-self-end">
+					<h1 className="font-impact text-3xl">Q3 2022</h1>
+				</div>
+				<div className="justify-self-center">
+					<Image src={divide} alt="divide" width={10} />
+				</div>
+				<div className="justify-self-start">
+					<ol style={{ listStyleType: "number" }}>
+						<li>Project setup</li>
+						<li>Whitepaper</li>
+						<li>Social media setup: Discord, Twitter, Medium</li>
+						<li>NFTs Concept & Design</li>
+						<li>Pre-launch Project</li>
+						<li>Pre-launch Campaign</li>
+						<li>Whitelisting</li>
+						<li>VCs package offering</li>
+						<li>1st AMA</li>
+					</ol>
+				</div>
 			</div>
-			<div className="mx-auto text-center">
-				<ul className="steps steps-vertical">
-					<li className="step step-primary"></li>
-					<li className="step step-primary"></li>
-					<li className="step"></li>
-				</ul>
+			<div className="grid grid-cols-3 items-center  ">
+				<div className="justify-self-end">
+					<ol style={{ listStyleType: "number" }}>
+						<li>Project Launch: NFTs minting phase 1: Whitelist & VC minting, public minting.</li>
+						<li>NFT Marketplace</li>
+						<li>Yield Farming (onchain investment)</li>
+						<li>Halal Audit (certification)</li>
+						<li>Marketplace Audit</li>
+						<li>Philanthrophy 1st action</li>
+						<li>Halalanft clothing brand</li>
+						<li>Halalanft Finance (yield optimizer)</li>
+					</ol>
+				</div>
+				<div className="justify-self-center">
+					<Image src={divide} alt="divide" width={10} />
+				</div>
+				<div className="justify-self-start">
+					<h1 className="text-3xl font-impact">Q4 2022</h1>
+				</div>
 			</div>
-			<div>
-				<ol style={{ listStyleType: "number" }}>
-					<li>Project Launch: NFTs minting phase 1: Whitelist & VC minting, public minting.</li>
-					<li>NFT Marketplace</li>
-					<li>Yield Farming (onchain investment)</li>
-					<li>Halal Audit (certification)</li>
-					<li>Marketplace Audit</li>
-					<li>Philanthrophy 1st action</li>
-					<li>Halalanft clothing brand</li>
-					<li>Halalanft Finance (yield optimizer)</li>
-				</ol>
-			</div>
-			<div className="mx-auto text-center">
-				<ul className="steps steps-vertical">
-					<li className="step step-primary"></li>
-					<li className="step step-primary"></li>
-					<li className="step"></li>
-				</ul>
-			</div>
-			<div>
-				<ol style={{ listStyleType: "number" }}>
-					<li>Strengthen security of Halala NFT Marketplace & Halalan Finance</li>
-					<li>Delegate and/or Run Validator</li>
-					<li>Setup DAO</li>
-					<li>Offchain Business Partnership</li>
-					<li>Decentralized Exchange (DEX)</li>
-					<li>Halalanft clothing brand</li>
-					<li>Halalan chain</li>
-					<li>Real-world use case for Halalan chain</li>
-					<li>Halalan token</li>
-				</ol>
+
+			<div className="grid grid-cols-3 items-center  ">
+				<div className="justify-self-end">
+					<h1 className="text-3xl font-impact">2023</h1>
+				</div>
+				<div className="justify-self-center">
+					<Image src={divide} alt="divide" width={10} />
+				</div>
+				<div className="justify-self-start">
+					<ol style={{ listStyleType: "number" }}>
+						<li>Strengthen security of Halala NFT Marketplace & Halalan Finance</li>
+						<li>Delegate and/or Run Validator</li>
+						<li>Setup DAO</li>
+						<li>Offchain Business Partnership</li>
+						<li>Decentralized Exchange (DEX)</li>
+						<li>Halalanft clothing brand</li>
+						<li>Halalan chain</li>
+						<li>Real-world use case for Halalan chain</li>
+						<li>Halalan token</li>
+					</ol>
+				</div>
 			</div>
 		</div>
 	);
--- landing-page/datas/ourTeam.js
+++ landing-page/datas/ourTeam.js
@@ -1,32 +1,32 @@
 export let ourTeam = [
 	{
-		id: "1",
+		image: "fida",
 		name: "Fida Munadzir",
 		title: "Sharia Compliance Advisor",
 		profile: "Fida Munadzir is a well-known islamic scholar in Indonesia. He has written 3 (three) books related to Islamic perspective on blockchain and cryptocurrencies",
 	},
 	{
-		id: "2",
+		image: "iwan",
 		name: "Iwan",
 		title: "Co-Founder & Business Development",
 		profile:
 			"Iwan is a Halal Web3 enthusiast, he has broad experience in business management and supply chain. Recently, he has interested in exploring crypto space especially in DeFi, NFT, and Tokenomics. Halalanft is his first project in the web3 space, and he believe Halalanft will be the first Halal NFT project that will bring long term benefit for Web3 communities globally",
 	},
 	{
-		id: "3",
+		image: "rama",
 		name: "Rama",
 		title: "Lead Operation",
 		profile:
 			"More than 10 years of experience in diverse organizations and communities, both in national and international level, engaging and connecting people from various background, supported his knowledge of project management, surely help him to manage several projects related to Halalanft",
 	},
 	{
-		id: "4",
+		image: "andika",
 		name: "Andika",
 		title: "Co-Founder & Lead Developer",
 		profile: "After several years working on blockchain and Web3 projects, Riyan realize that Web3 space need halal ecosystem to attract Muslim community to this emerging technology.",
 	},
 	{
-		id: "5",
+		image: "mirzam",
 		name: "Mirzam Avicena",
 		title: "UI/UX Designer",
 		profile:
