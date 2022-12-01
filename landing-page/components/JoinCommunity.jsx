e36a1e6b3901324c2be27418f769562695641677
mirzavic4869 <avicenamirzam@gmail.com>
2022-12-01 10:18:58 +0700

    finishing

--- landing-page/components/JoinCommunity.jsx
+++ landing-page/components/JoinCommunity.jsx
@@ -1,29 +1,34 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDiscord } from "@fortawesome/free-brands-svg-icons";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faMedium } from "@fortawesome/free-brands-svg-icons";
+import { faDiscord, faTwitter, faMedium } from "@fortawesome/free-brands-svg-icons";
 
 const JoinCommunity = () => {
 	return (
-		<div className="px-8 py-16  bg-[#F2F2F2]  w-screen">
+		<div id="community" className="px-8 py-16  bg-[#F2F2F2]  w-screen">
 			<h1 className="text-4xl font-impact mb-8 text-center">Join with Community</h1>
 			<p className="mb-16 lg:w-1/2 mx-auto text-center">
 				Building NFTs should always be about community. Thus, we are aiming to help the Web3 community with halal ecosystem in the DeFi world. If you want to discuss and chat with us, please follow and join our media below.
 			</p>
-			<div className="justify-center flex lg:flex-row flex-col gap-5 text-center ">
-				<button className=" max-sm:w-40 px-4 py-2 gap-2 bg-[#374C8C] hover:bg-[#283765] text-white rounded-md flex items-center mb-4">
-					<FontAwesomeIcon icon={faDiscord} className="w-8" />
-					<strong>DISCORD</strong>
-				</button>
-				<button className="max-sm:w-40 px-4 py-2 gap-2 bg-[#374C8C] hover:bg-[#283765] text-white rounded-md flex items-center mb-4">
-					<FontAwesomeIcon icon={faTwitter} className="w-8" />
-					<strong>TWITTER</strong>
-				</button>
-				<button className="max-sm:w-40 px-4 py-2 gap-2 bg-[#374C8C] hover:bg-[#283765] text-white rounded-md flex items-center mb-4">
-					<FontAwesomeIcon icon={faMedium} className="w-8" />
-					<strong>MEDIUM</strong>
-				</button>
+			<div className="justify-center flex md:flex-row flex-col gap-5 text-center ">
+				<a className="self-center" href="https://discord.com/invite/be5fnEqrZQ">
+					<button className=" max-sm:w-40 px-4 py-2 gap-2 bg-[#374C8C] hover:bg-[#283765] text-white rounded-md flex items-center justify-center self-center mb-4">
+						<FontAwesomeIcon icon={faDiscord} className="w-8" />
+						<strong>DISCORD</strong>
+					</button>
+				</a>
+
+				<a className="self-center" href="https://twitter.com/halalanft">
+					<button className="max-sm:w-40 px-4 py-2 gap-2 bg-[#374C8C] hover:bg-[#283765] text-white rounded-md flex items-center justify-center self-center mb-4">
+						<FontAwesomeIcon icon={faTwitter} className="w-[1.6rem]" />
+						<strong>TWITTER</strong>
+					</button>
+				</a>
+				<a className="self-center" href="#">
+					<button className="max-sm:w-40 px-4 py-2 gap-2 bg-[#374C8C] hover:bg-[#283765] text-white rounded-md flex items-center justify-center self-center mb-4">
+						<FontAwesomeIcon icon={faMedium} className="w-8" />
+						<strong>MEDIUM</strong>
+					</button>
+				</a>
 			</div>
 		</div>
 	);
--- landing-page/components/Navbar.jsx
+++ landing-page/components/Navbar.jsx
@@ -1,26 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import logo from "../assets/fix.png";
+import { Link } from "react-scroll";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+	const [nav, setNav] = useState(false);
+	const handleClick = () => {
+		setNav(!nav);
+	};
+
 	return (
-		<nav className="flex justify-between px-8  py-6  items-center w-screen">
+		<nav className="sticky top-0 left-0 bg-white opacity-70 flex justify-between px-8  py-6  items-center w-screen ">
 			<div>
 				<Image src={logo} alt="Logo" width={150} className="lg:w-[194px]" />
 			</div>
-			<div className="hidden space-x-8 lg:flex">
-				<a href="#">About</a>
-				<a href="#">Why Us</a>
-				<a href="#">Concept Art</a>
-				<a href="#">Team</a>
-				<a href="#">Roadmap</a>
-				<a href="#">Community</a>
+			{/* menu */}
+
+			<ul className="hidden md:flex space-x-8">
+				<li>
+					<Link to="about" smooth={true} duration={500} className="hover:text-[#FAD02C] cursor-pointer">
+						About
+					</Link>
+				</li>
+				<li>
+					<Link to="why us" smooth={true} duration={500} className="hover:text-[#FAD02C] cursor-pointer">
+						Why Us
+					</Link>
+				</li>
+				<li>
+					<Link to="concept art" smooth={true} duration={500} className="hover:text-[#FAD02C] cursor-pointer">
+						Concept Art
+					</Link>
+				</li>
+				<li>
+					<Link to="team" smooth={true} duration={500} className="hover:text-[#FAD02C] cursor-pointer">
+						Team
+					</Link>
+				</li>
+				<li>
+					<Link to="roadmap" smooth={true} duration={500} className="hover:text-[#FAD02C] cursor-pointer">
+						Roadmap
+					</Link>
+				</li>
+				<li>
+					<Link to="community" smooth={true} duration={500} className="hover:text-[#FAD02C] cursor-pointer">
+						Community
+					</Link>
+				</li>
+			</ul>
+
+			{/* hamburger */}
+			<div className={nav ? "md:hidden z-50 text-white" : "md:hidden z-50"} onClick={handleClick}>
+				{!nav ? <FaBars /> : <FaTimes />}
 			</div>
-			<div className="flex lg:hidden">
-				<svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-				</svg>
-			</div>
+			{/* mobile menu */}
+
+			<ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen  flex flex-col justify-center items-center bg-[#374C8C] z-20"}>
+				<li className="py-6 text-2xl text-white">
+					<Link to="about" smooth={true} duration={500} className="hover:text-[#374C8C] cursor-pointer">
+						About
+					</Link>
+				</li>
+				<li className="py-6 text-2xl text-white">
+					<Link to="why us" smooth={true} duration={500} className="hover:text-[#374C8C] cursor-pointer">
+						Why Us
+					</Link>
+				</li>
+				<li className="py-6 text-2xl text-white">
+					<Link to="concept art" smooth={true} duration={500} className="hover:text-[#374C8C] cursor-pointer">
+						Concept Art
+					</Link>
+				</li>
+				<li className="py-6 text-2xl text-white">
+					<Link to="team" smooth={true} duration={500} className="hover:text-[#374C8C] cursor-pointer">
+						Team
+					</Link>
+				</li>
+				<li className="py-6 text-2xl text-white">
+					<Link to="roadmap" smooth={true} duration={500} className="hover:text-[#374C8C] cursor-pointer">
+						Roadmap
+					</Link>
+				</li>
+				<li className="py-6 text-2xl text-white">
+					<Link to="community" smooth={true} duration={500} className="hover:text-[#374C8C] cursor-pointer">
+						Community
+					</Link>
+				</li>
+			</ul>
 		</nav>
 	);
 };
