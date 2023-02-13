4569aaea8d0359470f2c271278c0fab5aa3f5b09
mirzavic4869 <avicenamirzam@gmail.com>
2023-02-13 09:23:10 +0700

    change text color label

--- landing-page/pages/minting/components/SendTransaction.jsx
+++ landing-page/pages/minting/components/SendTransaction.jsx
@@ -31,21 +31,21 @@ export default function SendTransaction() {
 			>
 				<div class="grid gap-6 mb-6 md:grid-cols-2">
 					<div>
-						<label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+						<label for="address" class="block mb-2 text-sm font-medium text-gray-900">
 							Address
 						</label>
 						<input
 							id="address"
 							className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 							required
-							aria-label="Recipient"
+							aria-label="address"
 							onChange={(e) => setTo(e.target.value)}
 							placeholder="0xA0Cf…251e"
 							value={to}
 						/>
 					</div>
 					<div>
-						<label for="amount" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+						<label for="amount" className="block mb-2 text-sm font-medium text-gray-900 ">
 							Amount
 						</label>
 						<input
