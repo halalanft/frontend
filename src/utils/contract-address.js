8bf2c376bf230dc71086f8b88d6a9edd63fa6735
rizary <andika.riyan@gmail.com>
2023-05-20 16:30:53 +0700

    add distribute button, and all wallet halalanft

--- src/utils/contract-address.js
+++ src/utils/contract-address.js
@@ -10,6 +10,11 @@ export const USDC =
     ? ContractAddress.fuji.USDC
     : ContractAddress.avax.USDC
 
+export const TREASURY = '0xB85885977D5daf4D6768AFC670C14a5FC209b8E9'
+export const FILANTROFI = '0x6A07F68a60eB5890947A5af40F3eA928447d5527'
+export const MARKOPS = '0x42EF5475Eb5951B92cE28dccECB720654623fCb2'
+export const TEAMS = '0x742a1b678a34F9E79Ff4468968FFed99676ad3dc'
+
 export const truncate = (address) => {
   const match = address.match(/^(0x[a-zA-Z0-9])[a-zA-Z0-9]+([a-zA-Z0-9])$/)
   return match
